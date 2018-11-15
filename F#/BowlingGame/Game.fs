2d4b19df2928bf7599c2358d47387955257501ec
Joel Keyser <keyser.joel@gmail.com>
2018-11-14 19:50:25 -0600

    Finish implementing score for valid games.

--- F#/BowlingGame/Game.fs
+++ F#/BowlingGame/Game.fs
@@ -4,4 +4,24 @@
         List.append game [pins]
 
     let score (game:int list) =
-        List.sum game
+        let rec scoreFrame frameNumber (game:int list) =
+            if frameNumber = 10 then
+                List.sum game
+            else
+                let recurse = scoreFrame (frameNumber + 1)
+                match game with
+                | [] ->
+                    0
+                | fst::[] ->
+                    fst
+                | fst::snd::[] when fst = 10 ->
+                    fst + snd + recurse [snd]
+                | fst::snd::[] ->
+                    fst + snd
+                | fst::snd::tail when fst = 10 ->
+                    fst + snd + tail.Head + recurse (snd::tail)
+                | fst::snd::tail when fst + snd = 10 ->
+                    fst + snd + tail.Head + recurse tail
+                | fst::snd::tail ->
+                    fst + snd + recurse tail
+        scoreFrame 1 game
--- F#/BowlingGameTests/GameTests.fs
+++ F#/BowlingGameTests/GameTests.fs
@@ -19,3 +19,11 @@ let ``All ones scores 20`` () =
 [<Fact>]
 let ``Spare counts next roll twice`` () =
     assert_rolls_makes_score [4;6;3] 16
+
+[<Fact>]
+let ``Strike counts next two rolls twice`` () =
+    assert_rolls_makes_score [10;4;3] 24
+
+[<Fact>]
+let ``Perfect game scores 300`` () =
+    assert_rolls_makes_score [for i in 1..12 -> 10] 300
